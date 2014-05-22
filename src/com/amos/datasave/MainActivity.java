b6a4dc007a678c20abc6c9e6954add6c8b0b6b8a
liyayun <amosli@infomorrow.com>
2014-05-23 01:45:51 +0800

    add save password to sdcard Environment

--- src/com/amos/datasave/MainActivity.java
+++ src/com/amos/datasave/MainActivity.java
@@ -2,6 +2,9 @@ package com.amos.datasave;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StatFs;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -12,22 +15,28 @@ import android.widget.Toast;
 public class MainActivity extends Activity implements OnClickListener {
 
 	String tag = "MainActivity";
-	EditText et_name;//用户名
-	EditText et_password;//密码
-	Button bt_login;//登录按钮
-	CheckBox cb_password;//单选框
+	EditText et_name;// 用户名
+	EditText et_password;// 密码
+	Button bt_login;// 登录按钮
+	CheckBox cb_password;// 单选框
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//初始化
+		// 初始化
 		et_name = (EditText) this.findViewById(R.id.et_name);
 		et_password = (EditText) this.findViewById(R.id.et_password);
 		bt_login = (Button) this.findViewById(R.id.bt_login);
 		cb_password = (CheckBox) this.findViewById(R.id.cb_password);
+
+		StatFs statFs = new StatFs(Environment.getExternalStorageDirectory().getPath());
+		long blockSize = statFs.getBlockSize();
+		long blockCount = statFs.getBlockCount();
+		long sdCardSize = blockSize*blockCount;
+		Log.d(tag,""+sdCardSize );
 		
-		//注册点击事件
+		// 注册点击事件
 		bt_login.setOnClickListener(this);
 
 	}
@@ -38,10 +47,13 @@ public class MainActivity extends Activity implements OnClickListener {
 		switch (v.getId()) {
 		case R.id.bt_login:
 
-			if (cb_password.isChecked()) {//如果单选框被选中了
-				//保存数据到rom
-				new savePasswordService(this).savePasswordToFile(et_name.getText().toString(), et_password.getText().toString());
-				Toast.makeText(this, "保存数据成功!", Toast.LENGTH_SHORT).show();
+			if (cb_password.isChecked()) {// 如果单选框被选中了
+				// 保存数据到rom
+//				new savePasswordService(this).savePasswordToFile(et_name.getText().toString(), et_password.getText().toString());
+				
+				//save to sdcard
+				new savePasswordService(this).savePasswordToSDCard(et_name.getText().toString(), et_password.getText().toString());
+				//Toast.makeText(this, "保存数据成功!", Toast.LENGTH_SHORT).show();
 			} else {
 				Toast.makeText(this, "没有保存数据!", Toast.LENGTH_SHORT).show();
 			}
--- src/com/amos/datasave/savePasswordService.java
+++ src/com/amos/datasave/savePasswordService.java
@@ -1,10 +1,16 @@
 package com.amos.datasave;
 
+import java.io.File;
 import java.io.FileOutputStream;
 
 import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothClass.Device;
 import android.content.Context;
+import android.os.Environment;
+import android.os.StatFs;
+import android.provider.Settings;
 import android.util.Log;
+import android.widget.Toast;
 
 @SuppressLint("WorldWriteableFiles")
 public class savePasswordService {
@@ -27,6 +33,49 @@ public class savePasswordService {
 			fileOutput.close();
 		} catch (Exception e) {
 			Log.d(tag, "设置文件的读写权限失败!");
+			e.printStackTrace();
+		}
+	}
+	//写数据到sdcard
+	public void savePasswordToSDCard(String name, String password) {
+		// android 2.1 /sdcard/xx.txt
+		// android 2.2 /mnt/sdcard/xx.txt
+		// self_define /excard/xx.txt
+		
+//		File externalStorageDirectory = Environment.getExternalStorageDirectory();
+//		String path = externalStorageDirectory.getPath();
+//		System.out.println("path:" + path);
+
+		// 要存储的内容
+		String content = name + ":" + password;
+		
+		StatFs statFs = new StatFs(Environment.getExternalStorageDirectory().getPath());
+		long blockSize = statFs.getBlockSize();
+		long blockCount = statFs.getBlockCount();
+		long sdCardSize = blockSize*blockCount;
+		
+		Log.d(tag,""+sdCardSize );
+		System.out.println("sdCardSize:"+sdCardSize);
+		
+		Log.d(tag, "检验sdcard是否可用?");
+		//判断sdcard是否存在?
+		if(!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+			Log.d(tag, "sdcard不可用!");
+			Toast.makeText(context, "没有找到SDCard!", Toast.LENGTH_LONG);
+			return ;
+		};
+		
+		;
+		
+		try {
+			// File file = new File("/sdcard/qqpassword.txt");
+			// File file = new File(path + "/qqpassword2.txt");
+			File file = new File(Environment.getExternalStorageDirectory(), "/qqpassword2.txt");
+			FileOutputStream fos = new FileOutputStream(file);
+			fos.write(content.getBytes());
+			fos.flush();
+			fos.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
